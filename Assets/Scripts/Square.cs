6cd9b1291f7bec1566fe68f5f6a1e9ad83756026
birdham <139436177+birdham@users.noreply.github.com>
2024-04-17 15:22:21 +0900

    탄알_막아_버티는_게임

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-3.0f, 3.0f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        float size = Random.Range(0.5f, 1.5f);
+
+        transform.position = new Vector2 (x, y);
+
+        transform.localScale = new Vector2(size, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Balloon"))
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+}
